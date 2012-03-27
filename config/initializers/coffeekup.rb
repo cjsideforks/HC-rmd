6c76863605c47b3f2870bb38cd1c34505721606b
Charlie Magee <charlie@signaldesign.net>
2012-03-26 23:29:49 -0700

    deleted Coffeekup config file

--- config/initializers/coffeekup.rb
+++
@@ -1,7 +0,0 @@
-Coffeekup.configure do |config|
-  config.namespace = 'templates'
-  config.extension = 'ck'
-  config.base_directory = 'app/assets/javascripts/shared'
-end
-
-Coffeekup.register_engine
